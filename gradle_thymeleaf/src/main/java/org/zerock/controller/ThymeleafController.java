934549546f78e7b016d8e9b112d9b9de443e7229
baechan <asdfjklddd@gmail.com>
2021-10-07 11:14:53 +0900

    jenkins ci/cd pipeline 구축를 위한 github - sts 연동 확인

---
+++ gradle_thymeleaf/src/main/java/org/zerock/controller/ThymeleafController.java
@@ -0,0 +1,24 @@
+package org.zerock.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ThymeleafController {
+
+	@RequestMapping("/thymeleafTest")
+	public void sendData(Model model) {
+		List<String> list = new ArrayList<>();
+		
+		for(int i=0; i<20; i++) {
+			list.add("Data: " + i);
+		}
+		
+		model.addAttribute("name","Gradle_Thymeleaf Sample Data");
+		model.addAttribute("list", list);
+	}
+}
